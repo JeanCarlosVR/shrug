4e9d5ee23083e278b17461b9702314bc5447423d
Jean Carlos VR <46204992+JeanCarlosVR@users.noreply.github.com>
2021-01-25 13:31:41 -0500

    Delete config.ts

--- src/config.ts
+++
@@ -1,6 +0,0 @@
-export const client_users_with_admin_perms: string[] = ["YOUR_ID"];
-export const client_token: string = "DISCORD_BOT_TOKEN";
-
-export const client_prefix: string = "shrug!";
-
-export const mongo_atlas_url: string = "MONGO_URL"
