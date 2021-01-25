62b0399a91d847d70b7f2b86858d18d92a8e3920
Jean Carlos VR <46204992+JeanCarlosVR@users.noreply.github.com>
2021-01-25 13:22:12 -0500

    Update config.ts

--- src/config.ts
+++ src/config.ts
@@ -1,6 +1,6 @@
-export const client_users_with_admin_perms: string[] = ["525842461655040011"];
-export const client_token: string = "NzQxNTM5MzkwMTQwMDU1NjIy.Xy5CXw.9WONviayqsTDpDqg_15ikWlVp6w";
+export const client_users_with_admin_perms: string[] = ["YOUR_ID"];
+export const client_token: string = "DISCORD_BOT_TOKEN";
 
 export const client_prefix: string = "shrug!";
 
-export const mongo_atlas_url: string = "mongodb+srv://jean:1065126331@cluster0-qe6ou.mongodb.net/shrug?retryWrites=true&w=majority"
+export const mongo_atlas_url: string = "MONGO_URL"
